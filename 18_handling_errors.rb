ead6c985c1b0ca8f33b5b0d8f1905fb6448982f7
cloudkhoo96 <cloud7535@gmail.com>
2024-01-20 22:54:23 +0800

    done lesson 18_error_handling

---
+++ 18_handling_errors.rb
@@ -0,0 +1,17 @@
+lucky_nums = [4, 8, 15, 16, 23, 42]
+
+begin
+    num = 10 / 0
+rescue ZeroDivisionError => e
+    puts e
+    # puts "Division by zero error"
+end
+
+begin
+    lucky_nums["dog"]
+    num = 10 / 0
+rescue ZeroDivisionError
+    puts "Division by zero error"
+rescue TypeError => e
+    puts e
+end
