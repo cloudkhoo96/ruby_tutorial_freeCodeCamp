00fe02938e45b5810820745510921ea2fc3784b8
cloudkhoo96 <cloud7535@gmail.com>
2024-01-20 22:33:40 +0800

    done lesson 16_reading_file

---
+++ 16_reading_files.rb
@@ -0,0 +1,29 @@
+File.open("employees.txt", "r") do |file|
+
+    puts file
+    puts file.read()
+    puts file.read().include? "Jim"
+
+end    
+
+File.open("employees.txt", "r") do |file|
+
+    puts file.readline()
+    puts file.readchar()
+
+end    
+
+File.open("employees.txt", "r") do |file|
+
+    for line in file.readlines()
+        puts line
+    end
+
+end    
+
+
+file = File.open("employees.txt", "r")
+
+puts file.read
+
+file.close()
